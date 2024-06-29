830454a5077a9317e4ad23938be89e3ffe01b33f
PANTRERA <a460299660@uvm.edu.mx>
2024-06-29 17:16:52 -0600

    añadi mi nombre

---
+++ P1_Conexion/Form1.cs
@@ -0,0 +1,64 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P1_Conexion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnConectar_Click(object sender, EventArgs e)
+        {
+            String servidor = txtServidor.Text;
+            String puerto = txtPuerto.Text;
+            String usuario = txtUsuario.Text;
+            String password = txtPassword.Text;
+            String bd = txtBD.Text;
+
+            //Sentencia de conexion
+            String cadenadeconexion = "Database=" + bd + ";DataSource=" + servidor + ";Port=" + puerto + ";User id=" + usuario + ";Password=" + password;
+
+            //Comandos de conexion
+            MySqlConnection conexion = new MySqlConnection(cadenadeconexion);
+            MySqlDataReader reader = null;
+            String data = null;
+        
+            try{
+                String consulta = "HA CONECTADO A LA BASE DE DATOS CORRECTAMENTE";
+                MySqlCommand cmd = new MySqlCommand(consulta);
+                cmd.Connection = conexion;
+                conexion.Open();
+                reader = cmd.ExecuteReader();
+
+                while(reader.Read())
+                {
+                    data += reader.GetString(0);
+                }
+                MessageBox.Show(data);
+            }catch (MySqlException ex) 
+            {
+                MessageBox.Show("Error al conectar"+ex.Message);
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+    }
+}
---
+++ RegistroProductos/Form2.cs
@@ -0,0 +1,119 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegistroProductos
+{
+    public partial class Form2 : Form
+    {
+        private string productoId;
+        private string codigo;
+        private string nombre;
+        private string descripcion;
+        private decimal precio;
+        private decimal costoc;
+        private int existencias;
+
+  
+            public Form2(string id, string codigo, string nombre, string descripcion, decimal precio, decimal costoc, int existencias)
+        {
+            InitializeComponent();
+            this.productoId = id;
+            this.codigo = codigo;
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+            this.precio = precio;
+            this.costoc = costoc;
+            this.existencias = existencias;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            string idUsuario = txtIdUsuario.Text.Trim();
+            if (string.IsNullOrEmpty(idUsuario))
+            {
+                MessageBox.Show("Por favor, ingrese un codigo de usuario.");
+                return;
+            }
+            bool codeValid = CheckUserCode(idUsuario);
+            if (!codeValid)
+            {
+                MessageBox.Show("Código inválido.  NO Puede actualizar los productos.");
+                this.DialogResult = DialogResult.Cancel;
+                return;
+            }
+
+            using (MySqlConnection conn = Conexion.conexion())
+            {
+
+
+                try
+                {
+                    conn.Open();
+
+                    var query = "UPDATE productos SET Nombre =@nombre, Precio_publico = @Precio, Costo_compra = @Costo, Existencias = @Existencias WHERE idProductos = @id";
+                  var command = new MySqlCommand(query, conn);
+
+                    command.Parameters.AddWithValue("@id", productoId);
+                    command.Parameters.AddWithValue("@Nombre", nombre);
+                    command.Parameters.AddWithValue("@Precio", precio);
+                    command.Parameters.AddWithValue("@Costo", costoc);
+                    command.Parameters.AddWithValue("@Existencias", existencias);
+
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Producto actualizado correctamente.");
+                        this.DialogResult = DialogResult.OK;
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se encontró el producto con el ID especificado.");
+                        this.DialogResult = DialogResult.Cancel;
+                    }
+
+                    this.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al modificar: " + ex.Message);
+                }
+            }
+        }
+
+        private bool CheckUserCode(string idUsuario)
+        {
+            using (MySqlConnection conn = Conexion.conexion())
+            {
+                conn.Open();
+                string userQuery = "SELECT COUNT(*) FROM Usuarios WHERE IdUsuario = @IdUsuario AND TienePermisos = 1";
+                MySqlCommand userCommand = new MySqlCommand(userQuery, conn);
+                userCommand.Parameters.AddWithValue("@IdUsuario", idUsuario);
+
+                int userCount = Convert.ToInt32(userCommand.ExecuteScalar());
+                return userCount > 0;
+            }
+        }
+    }
+}
+
+
